20efe4c4335c8b3854f3c76425a9db93cccf3614
miaque <miaque@163.com>
2019-03-30 18:53:28 +0800

    初始化仓库

--- frontend/src/App.vue
+++ frontend/src/App.vue
@@ -1,45 +1,27 @@
 <template>
   <div id="app">
-    <div id="nav">
-      <router-link to="/">Hello</router-link> |
-      <router-link to="/callservice">Service</router-link> |
-      <router-link to="/bootstrap">Bootstrap</router-link> |
-      <router-link to="/user">User</router-link>
-    </div>
-    <router-view :hellomsg="msg"></router-view>
+    <router-view/>
   </div>
 </template>
 
 <script>
-
 export default {
-  name: 'app',
-  data () {
-    return {
-      msg: 'Welcome to your Vue.js powered Spring Boot App'
-    }
-  }
+  name: 'App'
 }
 </script>
 
-<style lang="scss">
-#app {
-  font-family: 'Avenir', Helvetica, Arial, sans-serif;
-  -webkit-font-smoothing: antialiased;
-  -moz-osx-font-smoothing: grayscale;
-  text-align: center;
-  color: #2c3e50;
-  margin-top: 20px;
+<style lang="less">
+.size{
+  width: 100%;
+  height: 100%;
 }
-
-#nav {
-  padding: 30px;
-  a {
-    font-weight: bold;
-    color: #2c3e50;
-    &.router-link-exact-active {
-     color: #42b983;
-    }
-  }
+html,body{
+  .size;
+  overflow: hidden;
+  margin: 0;
+  padding: 0;
+}
+#app {
+  .size;
 }
 </style>
